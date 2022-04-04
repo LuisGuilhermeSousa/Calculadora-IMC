3c8147f1eff4076bd90cb77da8d3298d34697c8d
Luis Guilherme <56441318+LuisGuilhermeSousa@users.noreply.github.com>
2022-04-03 23:38:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './app.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [peso, setPeso] = useState('')
+  const [altura, setAltura] = useState('')
+  const [mensagem, setMensagem] = useState('')
+
+  function calculoIMC() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18.6){
+      setMensagem("Você está abaixo do peso! Seu IMC é : " + imc.toFixed(2))
+    }
+    else if(imc >= 18.6 && imc < 24.9) {
+      setMensagem("Peso ideial! Seu IMC é : " + imc.toFixed(2))
+    }
+    else if(imc >= 24.9 && imc < 34.9) {
+      setMensagem("Você está levemente acima do peso! Seu IMC é : " + imc.toFixed(2))
+    }
+    else if(imc > 34.9) {
+      setMensagem("Cuidado, obesidade! Seu IMC é : " + imc.toFixed(2))
+    }
+  }
+
+  return(
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC</span>
+      <div className="area-input">
+
+        <h2>Peso:</h2>
+        <input
+          type="text"
+          placeholder="Peso em (kg) Ex: 90"
+          value={peso}
+          onChange={ (e) => setPeso(e.target.value)}
+        />
+
+        <h2>Altura:</h2>
+        <input
+          type="text"
+          placeholder="Altura em (cm) Ex: 180"
+          value={altura}
+          onChange={ (e) => setAltura(e.target.value)}
+        />
+
+        <button onClick={calculoIMC}>
+          Calcular
+        </button>
+
+      </div>
+      <h2>{mensagem}</h2>
     </div>
-  );
+  )
 }
 
 export default App;
